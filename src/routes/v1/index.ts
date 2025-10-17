0b99158b2851c16d037ac945353f2e62a4fa6587
Tariq Monowar <hassainseam@gmail.com>
2025-10-17 16:28:48 +0600

    fix

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,19 +2,19 @@ import { FastifyInstance } from "fastify";
 import auth from "./auth/auth.routes";
 import meetings from "./meetings/meetings.route";
 import nomination from "./nomination/nomination.routes";
-import schedule from "./schedule/schedule.route";
- 
+
 import dashboard from "./dashboard/dashboard.route";
 import message from "./message/message.routes";
 import notification from "./notification/notification.routes"
 import users from "./users/users.routes";
+import scheduleRoutes from "./schedule/schedule.routes";
 
 async function routesV1(fastify: FastifyInstance) {
   const moduleRoutes = [
     { path: "/auth", route: auth },
     { path: "/meetings", route: meetings },
     { path: "/nomination", route: nomination },
-    { path: "/schedule", route: schedule },
+    { path: "/schedule", route: scheduleRoutes },
     { path: "/dashboard", route: dashboard },
     { path: "/message", route: message },
     { path: "/notification", route: notification },
