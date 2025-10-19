2255b3d2614e30468c7577380ccf10cab0403bdf
Tariq Monowar Hosain <101199109+Tariq-Monowar@users.noreply.github.com>
2025-10-19 11:40:06 +0600

    Delete build directory

--- build/src/utils/baseurl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseUrl = exports.getImageUrl = void 0;
-const getImageUrl = (imagePath) => {
-    return `${process.env.BASE_URL}${imagePath}`;
-};
-exports.getImageUrl = getImageUrl;
-exports.baseUrl = process.env.BASE_URL;
-//# sourceMappingURL=baseurl.js.map
--- build/src/utils/downloadImage.utils.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=downloadImage.utils.js.map
